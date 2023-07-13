7fed3264abfdb39942f3e47151ce41c88cd05b80
iPegii <51372604+iPegii@users.noreply.github.com>
2023-07-13 16:34:43 +0300

    update tests, remove unused snapshots

--- frontend/src/tests/pw-tests/course/course-page.spec.js
+++ frontend/src/tests/pw-tests/course/course-page.spec.js
@@ -9,6 +9,11 @@ test.describe("Course page", () => {
 test('Renders matrix page', async ({ page }) => {
   await page.goto('http://localhost:8080/course/1');
   const courseHeader = page.locator('id=course-header')
+  await expect(page.getByRole('cell', { name: 'taitotasot' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'osio' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'Yhtälöryhmät' })).toBeVisible()
+  await expect(page.getByText('Osaan muokata yhtälöryhmää vastaavan matriisin alkeisrivitoimituksilla redusoidu')).toBeVisible()
+  await expect(page.getByTestId('matrix-table').locator('div').filter({ hasText: '1-2' }).nth(2)).toBeVisible()
   await expect(courseHeader).toHaveText('Lineaarialgebra ja matriisilaskenta I kevät 2018')
   await expect(page).toHaveScreenshot();
 })
